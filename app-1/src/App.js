1e306bfa43c2b4eaf31b8801f8cffb108950a21b
Root <robhunn74@gmail.com>
2018-06-25 22:31:59 -0500

    forgot to push at 5 little late sorry

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      textfield: '',
+      header: 'Change Me'
+    }
+
+    this.updateHeader = this.updateHeader.bind(this)
+  }
+
+
+
+
+updateHeader(val) {
+  this.setState({ header: val });
+  }
+
+
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="wraper"> 
+      <h1>Type here!!!</h1>
+      <input id="header-input" onChange={ (e) => { this.updateHeader(e.target.value); }}/>
+        <h1 id="header"> {this.state.header} </h1>
       </div>
     );
   }
--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+
+  constructor(props){
+    super(props);
+    this.state={
+      foo:[
+        "spaghetti",
+        "ice cream",
+        "sushi",
+        "bologna",
+        "cheese"
+      ]
+    }
+
+  }
+
+
+
+  render(){
+
+    
+    let foo = this.state.foo.map( (element, index) => {
+      return (
+        <h2 key={ index }>{ element }</h2>
+      )
+    })
+ 
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      { foo }
       </div>
     );
   }
